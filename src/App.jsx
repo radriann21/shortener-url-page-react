babf208ab5dbe05f8d5db6374b294d6ae89af4d3
radriann21 <adrianrangelargu@gmail.com>
2025-01-04 20:47:44 -0400

    remove App.css and react.svg; add Hero component with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+
 function App() {
   return (
     <main className="max-w-full min-h-screen bg-white overflow-hidden relative">
       <section className="max-w-7xl mx-auto p-8 md:p-12">
         <Header />
+        <Hero />
       </section>
     </main>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import illustration from "../assets/images/illustration-working.svg";
+
+export const Hero = () => {
+  return (
+    <section className="mt-16 sm:mt-24 flex flex-col-reverse sm:flex-row items-center justify-between">
+      <div className="mt-8 sm:mt-0">
+        <h1 className="font-bold text-4xl sm:text-7xl text-neutral-very-dark-blue text-center sm:text-left">
+          More than just shorter links
+        </h1>
+        <p className="mt-2 text-neutral-grayish-violet w-[80%] sm:w-[60%] text-center sm:text-lg sm:text-left mx-auto sm:mx-0">
+          Build your brand&apos;s recognition and get detailed insights on how
+          your links are performing.
+        </p>
+        <button className="mt-8 py-3 px-10 bg-primary-custom-cyan rounded-full font-bold text-white transition-colors duration-300 hover:bg-opacity-60 translate-x-[50%] sm:translate-x-0 sm:mx-0">
+          Get Started
+        </button>
+      </div>
+      <div>
+        <img className="w-full" src={illustration} alt="illustration" />
+      </div>
+    </section>
+  );
+};
